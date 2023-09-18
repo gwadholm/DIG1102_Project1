f3d9474bfc14e733731a6a68dbc10ac77a37d960
gwadholm <gwadholm@gmail.com>
2023-09-18 12:22:42 -0400

    Changed code to use array to assign a letter for computer choice

--- script.js
+++ script.js
@@ -1,28 +1,54 @@
 let totalWin = 0;
 let totalLoss = 0;
 let totalTie = 0;
+let computerLetterChoice = ["r", "p", "s"];
 
 let gameCount = Number(prompt("How many rounds do you want to play?"));
 
 for (i=0; i < gameCount; i++) {
-  let userChoice = prompt("Choose r for rock, p for paper, or s for scissors");
-  let computerChoice = Math.round(Math.random() * (2));
-
-  if ((userChoice === "r" && computerChoice === 0) || (userChoice === "p" && computerChoice === 1) || (userChoice === "s" && computerChoice === 2) ) {
-    alert("It's a tie!");
-    totalTie++;
-  } else if ((userChoice === "r" && computerChoice === 2) || (userChoice === "p" && computerChoice === 0) || (userChoice === "s" && computerChoice === 1)) {
-    alert("You win!");
-    totalWin++;
-  } else if ((userChoice === "r" && computerChoice === 1) || (userChoice === "p" && computerChoice === 2) || (userChoice === "s" && computerChoice === 0)) {
-    alert("You lose, sir!");
-    totalLoss++;
+  let userChoice = prompt("Pick rock, paper or scissors (enter r, p or s)");
+  let computerChoice = computerLetterChoice[Math.round(Math.random() * (2))];
+  let alertMessageTie = "It's a tie! You picked " + userChoice + " and the computer picked " + computerChoice 
+  let alertMessageWin = "You won! You picked " + userChoice + " and the computer picked " + computerChoice 
+  let alertMessageLose = "Sorry, you lost. You picked " + userChoice + " and the computer picked " + computerChoice 
+  
+  if (userChoice === "r") {
+    if (computerChoice == "r") {
+      alert(alertMessageTie);
+      totalTie++;
+    } else if (computerChoice == "p") {
+      alert(alertMessageLose);
+      totalLoss++;
+    } else if (computerChoice == "s") {
+      alert("You won! You picked " + userChoice + " which beats s");
+      totalWin++;
+    }
+  } else if (userChoice === "p") {
+    if (computerChoice == "r") {
+      alert(alertMessageWin);
+      totalWin++;
+    } else if (computerChoice == "p") {
+      alert("It's a tie!");
+      totalTie++;
+    } else if (computerChoice == "s") {
+      alert(alertMessageLose);
+      totalLoss++;
+    }
+  } else if (userChoice === "s") {
+    if (computerChoice == "r") {
+      alert(alertMessageLose);
+      totalLoss++;
+    } else if (computerChoice == "p") {
+      alert(alertMessageWin);
+      totalWin++;
+    } else if (computerChoice == "s") {
+      alert("Its a tie!");
+      totalTie++;
+    } 
   } else {
     alert("Please only enter r, p, or s as your choice");
     i--;
   }
 } 
 
-alert("Games won: " + totalWin);
-alert("Games lost: " + totalLoss);
-alert("Games tied: " + totalTie);
+alert("Games won: " + totalWin + " | Games lost: " + totalLoss + " | Games tied: " + totalTie);
